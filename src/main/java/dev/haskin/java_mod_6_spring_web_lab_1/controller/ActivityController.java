2a09f6b5bcf3e7f537af7ba734d9b00aeb7200c8
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-05 19:37:30 -0400

    added @repository to activity

--- src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/controller/ActivityController.java
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/controller/ActivityController.java
@@ -1,15 +1,24 @@
 package dev.haskin.java_mod_6_spring_web_lab_1.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.haskin.java_mod_6_spring_web_lab_1.dto.ActivityDTO;
+import dev.haskin.java_mod_6_spring_web_lab_1.repository.ActivityRepository;
 
 @RestController
 @RequestMapping("/activities")
 public class ActivityController {
 
+    @Autowired
+    ActivityRepository activityRepository;
+
     @GetMapping
-    String getActivities() {
-        return "new ArrayList<>()";
+    List<ActivityDTO> getActivities() {
+
     }
 }
--- src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/repository/CamperRepository.java
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/repository/CamperRepository.java
@@ -1,5 +1,9 @@
 package dev.haskin.java_mod_6_spring_web_lab_1.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.haskin.java_mod_6_spring_web_lab_1.model.Camper;
+
 public interface CamperRepository extends JpaRepository<Camper, Long> {
 
 }
