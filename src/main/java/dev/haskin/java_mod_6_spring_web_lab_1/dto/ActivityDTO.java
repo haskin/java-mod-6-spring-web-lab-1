3d1fae52009f499c2c3c1cef2bfeeb37bda72e4c
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-05 19:24:24 -0400

    created DTO objects

---
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/dto/ActivityDTO.java
@@ -0,0 +1,10 @@
+package dev.haskin.java_mod_6_spring_web_lab_1.dto;
+
+import lombok.Data;
+
+@Data
+public class ActivityDTO {
+    private Long id;
+    private String name;
+    private int difficulty;
+}
---
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/dto/SignupDTO.java
@@ -0,0 +1,5 @@
+package dev.haskin.java_mod_6_spring_web_lab_1.dto;
+
+public class SignupDTO {
+
+}
