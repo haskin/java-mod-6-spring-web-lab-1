d822e8e248f32b2d9929c020fddd67a872aef72b
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-06 13:56:57 -0400

    created signup service

--- src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/service/SignupService.java
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/service/SignupService.java
@@ -1,9 +1,30 @@
 
 package dev.haskin.java_mod_6_spring_web_lab_1.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import dev.haskin.java_mod_6_spring_web_lab_1.dto.SignupDTO;
+import dev.haskin.java_mod_6_spring_web_lab_1.model.Signup;
+import dev.haskin.java_mod_6_spring_web_lab_1.repository.SignupRepository;
 
 @Service
 public class SignupService {
+    @Autowired
+    SignupRepository signupRepository;
+    @Autowired
+    ModelMapper modelMapper;
 
+    SignupDTO createSignup(SignupDTO signupDTO) {
+        Signup signup = modelMapper.map(signupDTO, Signup.class);
+        try {
+            signup = signupRepository.save(signup);
+            return modelMapper.map(signup, SignupDTO.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "validation errors");
+        }
+    }
 }
