8603cdf131c1ba03865eb9145baa15925dc364a1
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-06 13:43:26 -0400

    added camper service class

--- src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/controller/CamperController.java
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/controller/CamperController.java
@@ -1,9 +1,7 @@
 package dev.haskin.java_mod_6_spring_web_lab_1.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,54 +12,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import dev.haskin.java_mod_6_spring_web_lab_1.dto.ActivityDTO;
 import dev.haskin.java_mod_6_spring_web_lab_1.dto.CamperDTO;
-import dev.haskin.java_mod_6_spring_web_lab_1.model.Activity;
 import dev.haskin.java_mod_6_spring_web_lab_1.model.Camper;
-import dev.haskin.java_mod_6_spring_web_lab_1.model.Signup;
-import dev.haskin.java_mod_6_spring_web_lab_1.repository.CamperRepository;
-import dev.haskin.java_mod_6_spring_web_lab_1.util.MapperUtil;
+import dev.haskin.java_mod_6_spring_web_lab_1.service.CamperService;
 
 @RestController
 @RequestMapping("/campers")
 public class CamperController {
-
     @Autowired
-    CamperRepository camperRepository;
-
-    @Autowired
-    ModelMapper modelMapper;
-
-    @Autowired
-    MapperUtil mapperUtil;
+    CamperService camperService;
 
     @GetMapping
     public List<CamperDTO> getAllCampers() {
-        return mapperUtil.mapList(camperRepository.findAll(), CamperDTO.class);
+        return camperService.readAllCampers();
     }
 
     @GetMapping("/{id}")
     public CamperDTO getCamperById(@PathVariable Long id) throws Exception {
-        Camper camper = camperRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Camper not found"));
-        CamperDTO camperDTO = modelMapper.map(camper, CamperDTO.class);
-        List<Signup> signups = camper.getSignups();
-        List<Activity> activities = signups.stream().map(Signup::getActivity).collect(Collectors.toList());
-        camperDTO.setActivities(
-                activities.stream()
-                        .map(activity -> modelMapper.map(activity, ActivityDTO.class))
-                        .collect(Collectors.toList()));
-        return camperDTO;
+        return camperService.readCamperById(id);
     }
 
     @PostMapping
     public CamperDTO postCamper(@RequestBody CamperDTO camperDTO) {
-        Camper camper = modelMapper.map(camperDTO, Camper.class);
-        try {
-            camper = camperRepository.save(camper);
-            return modelMapper.map(camper, CamperDTO.class);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "validation errors");
-        }
+        return camperService.createCamper(camperDTO);
     }
 }
--- src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/service/CamperService.java
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/service/CamperService.java
@@ -1,2 +1,56 @@
 
 package dev.haskin.java_mod_6_spring_web_lab_1.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import dev.haskin.java_mod_6_spring_web_lab_1.dto.ActivityDTO;
+import dev.haskin.java_mod_6_spring_web_lab_1.dto.CamperDTO;
+import dev.haskin.java_mod_6_spring_web_lab_1.model.Activity;
+import dev.haskin.java_mod_6_spring_web_lab_1.model.Camper;
+import dev.haskin.java_mod_6_spring_web_lab_1.model.Signup;
+import dev.haskin.java_mod_6_spring_web_lab_1.repository.CamperRepository;
+import dev.haskin.java_mod_6_spring_web_lab_1.util.MapperUtil;
+
+@Service
+public class CamperService {
+
+    @Autowired
+    CamperRepository camperRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    MapperUtil mapperUtil;
+
+    public List<CamperDTO> readAllCampers() {
+        return mapperUtil.mapList(camperRepository.findAll(), CamperDTO.class);
+    }
+
+    public CamperDTO readCamperById(Long id) {
+        Camper camper = camperRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Camper not found"));
+        CamperDTO camperDTO = modelMapper.map(camper, CamperDTO.class);
+        List<Signup> signups = camper.getSignups();
+        List<Activity> activities = signups.stream().map(Signup::getActivity).collect(Collectors.toList());
+        camperDTO.setActivities(mapperUtil.mapList(activities, ActivityDTO.class));
+        return camperDTO;
+    }
+
+    public CamperDTO createCamper(CamperDTO camperDTO) {
+        Camper camper = modelMapper.map(camperDTO, Camper.class);
+        try {
+            camper = camperRepository.save(camper);
+            return modelMapper.map(camper, CamperDTO.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "validation errors");
+        }
+    }
+}
