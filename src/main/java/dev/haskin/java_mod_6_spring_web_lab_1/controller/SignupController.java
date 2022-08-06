268bc458dc9f5381c74c4ee45f26eaf7564ab28c
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-06 16:52:54 -0400

    fixed typo in signup controller

--- src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/controller/SignupController.java
+++ src/main/java/dev/haskin/java_mod_6_spring_web_lab_1/controller/SignupController.java
@@ -2,12 +2,14 @@ package dev.haskin.java_mod_6_spring_web_lab_1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.haskin.java_mod_6_spring_web_lab_1.dto.SignupDTO;
 import dev.haskin.java_mod_6_spring_web_lab_1.service.SignupService;
 
 @RestController
+@RequestMapping("/signup")
 public class SignupController {
     @Autowired
     SignupService signupService;
